849c5b345411655898054557ca67a2e08fbeb0aa
Ferenc <ferenc.horvay@web.de>
2023-01-13 17:13:19 +0100

    add some different application profile properties and the docker container extension

--- rest-book/src/main/java/org/ferenco/quarkus/starting/BookRepository.java
+++ rest-book/src/main/java/org/ferenco/quarkus/starting/BookRepository.java
@@ -3,14 +3,18 @@ package org.ferenco.quarkus.starting;
 import java.util.List;
 import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class BookRepository {
 
+  @ConfigProperty(name = "books.genre", defaultValue = "Action")
+  String genre;
+
   public List<Book> getAllBooks() {
     return List.of(
-        new Book(1, "Pls help me", "Ferenco", 2023, "Thriller"),
-        new Book(2, "Pls help me the return of da boy", "Ferenco", 2023, "Thriller"));
+        new Book(1, "Pls help me", "Ferenco", 2023, genre),
+        new Book(2, "Pls help me the return of da boy", "Ferenco", 2023, genre));
   }
 
   public int countAllBooks() {
--- rest-book/src/test/java/org/ferenco/quarkus/starting/BookResourceIT.java
+++ rest-book/src/test/java/org/ferenco/quarkus/starting/BookResourceIT.java
@@ -1,8 +1,29 @@
 package org.ferenco.quarkus.starting;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
 public class BookResourceIT extends BookResourceTest {
-    // Execute the same tests but in packaged mode.
+
+  //We override the method because there are different values in the native image which has the prod profile loaded 
+  @Test
+  public void testGetBook() {
+    given()
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+        .pathParams("id", 1)
+        .when()
+        .get("/api/books/{id}")
+        .then()
+        .statusCode(200)
+        .body("title", is("Pls help me"))
+        .body("yearOfPublication", is(2023))
+        .body("genre", is("Drama"))
+        .body("author", is("Ferenco"));
+  }
 }
